b456158885a1972809a2b1be699d47b1de47e9ad
k4ntin <forand.quentin@gmail.com>
2021-10-20 20:24:05 +0200

    chore(setup): preparing node w/ ts

---
+++ nyc.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "all": true,
+  "check-coverage": true,
+  "include":          ["src"],
+  "reporter":         [ "lcov", "text" ],
+  "branches":   100,
+  "lines":      100,
+  "functions":  100,
+  "statements": 100,
+  "per-file": true,
+}
